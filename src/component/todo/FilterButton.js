66cf19ced4ad3b11290136e5d950ad11a2588337
KumarThirunavukkarasu <thirunavukkarasupk@gmail.com>
2022-08-15 18:32:32 +0530

    hideFilterButton

--- src/component/todo/FilterButton.js
+++ src/component/todo/FilterButton.js
@@ -1,14 +1,17 @@
 import React from 'react';
+import TodoManager from '../../services/TodoManager';
 
 const FilterButton = (context) => {
-	const { actions, data: filter } = context;
+	const { actions, state: { todoList }, data: filter } = context;
+	const noTodoList = TodoManager.getCountOfTodoList(todoList) === 0;
 
-	return (
-		<button
+	return !noTodoList
+		&& <button
 			onClick={ () => actions.filterButton(filter) }
-		>
+			// eslint-disable-next-line no-mixed-spaces-and-tabs
+		   >
 			{ filter }
-		</button>);
+		</button>;
 };
 
 export default FilterButton;
