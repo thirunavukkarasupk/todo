3d9aedba16efdbc54a1e78a6a3c65db87994a2fd
KumarThirunavukkarasu <thirunavukkarasupk@gmail.com>
2022-08-17 13:08:38 +0530

    refactoredSomeFiles

--- src/App.js
+++ src/App.js
@@ -1,20 +1,10 @@
 import { React } from 'react';
 import './App.scss';
-import TodoInput from './component/todo/TodoInput';
-import TodoList from './component/todo/TodoList';
-import ToggleAll from './component/todo/ToggleAllCheckBox';
-import ClearButton from './component/todo/ClearButton';
-import FilterBar from './component/todo/FilterBar';
-import ActionButton from './component/todo/ActionButton';
+import TodoPane from './component/todo/Index';
 
 const App = (context) =>
 	<div className="App" role="App">
-		<ToggleAll { ...context }/>
-		<TodoInput { ...context }/>
-		<ActionButton { ...context }/>
-		<TodoList { ...context }/>
-		<ClearButton { ...context }/>
-		<FilterBar { ...context }/>
+		<TodoPane { ...context }/>
 	</div>;
 
 export default App;
---
+++ src/component/TodoPane/Index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import TodoInput from './TodoInput';
+import TodoList from './TodoList';
+import ToggleAll from './ToggleAllCheckBox';
+import ClearButton from './ClearButton';
+import FilterBar from './FilterBar';
+import ActionButton from './ActionButton';
+
+const TodoPane = (context) =>
+	<div>
+		<ToggleAll { ...context }/>
+		<TodoInput { ...context }/>
+		<ActionButton { ...context }/>
+		<TodoList { ...context }/>
+		<ClearButton { ...context }/>
+		<FilterBar { ...context }/>
+	</div>;
+
+export default TodoPane;
