61cc71d40e8d0bdd64ebf2031cc1e60dd33c4cef
KumarThirunavukkarasu <thirunavukkarasupk@gmail.com>
2022-09-01 14:56:35 +0530

    TestCompletedForToggleTodo

--- src/services/todoManager.test.js
+++ src/services/todoManager.test.js
@@ -1,7 +1,16 @@
 import TodoManager from './TodoManager';
 
 describe('todoManager', () => {
-	const { addTodo, removeTodo } = TodoManager;
+	const { addTodo, removeTodo, toggleTodo } = TodoManager;
+	const todoList = [{
+		id: 'DDYB',
+		todo: 'Test The Code',
+		completed: false,
+	}, {
+		id: 'IJKL',
+		todo: 'Fix The Bugs',
+		completed: true,
+	}];
 
 	test('add todo to the todoList', () => {
 		const context = {
@@ -25,17 +34,26 @@ describe('todoManager', () => {
 	test('remove the todo from the todoList', () => {
 		const context = {
 			state: {
-				todoList: [{
-					id: 'DDYB',
-					todo: 'Test The Code',
-					completed: false,
-				}],
+				todoList,
 			},
 			data: { id: 'DDYB' },
 		};
 
 		const result = removeTodo(context);
 
-		expect(result).toEqual([]);
+		expect(result).toEqual([todoList[1]]);
+	});
+
+	test('toggle The Todo', () => {
+		const context = {
+			state: {
+				todoList,
+			},
+			data: { id: 'DDYB', completed: false },
+		};
+		const result = toggleTodo(context);
+
+		expect(result)
+			.toEqual([{ ...todoList[0], completed: true }, todoList[1]]);
 	});
 });
