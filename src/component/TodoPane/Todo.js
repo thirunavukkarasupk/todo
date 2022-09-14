0003425754e4c40e33181ef36bee5ad97ce9f08b
KumarThirunavukkarasu <thirunavukkarasupk@gmail.com>
2022-09-14 16:58:32 +0530

    TestCompletedForTodo

--- src/component/TodoPane/Todo.js
+++ src/component/TodoPane/Todo.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import RemoveButton from './RemoveButton';
 import CheckBox from './CheckBox';
+import TodoManager from '../../services/TodoManager';
 
 const Todo = (context) => {
-	const { data, actions, state: { editing }} = context;
+	const { data, actions } = context;
 	const { todo, completed } = data;
 
 	return (
-		<div className={ completed ? 'todo-completed' : 'todo-active' }>
+		<div
+			role="todo"
+			className={ completed ? 'todo-completed' : 'todo-active' }
+		>
 			<CheckBox { ...context }/>
 			<span
-				className={ editing !== null && editing.id === data.id
-					? 'edit-active'
-					: 'edit-inactive' }
+				role="todoText"
+				className={ TodoManager.getEditClassName(context) }
 				onClick={ () => actions.setEditing(data) }
 			>{todo}</span>
 			<RemoveButton { ...context }/>
---
+++ src/component/TodoPane/todo.test.js
@@ -0,0 +1,66 @@
+import Todo from './Todo';
+import React from 'react';
+import * as CheckBox from './CheckBox';
+import * as RemoveButton from './RemoveButton';
+import { render, fireEvent } from '@testing-library/react';
+import TodoManager from '../../services/TodoManager';
+
+describe('todo', () => {
+	test('when the completed is false', () => {
+		const context = {
+			state: { editing: null },
+			data: {
+				todo: Symbol('todo'),
+				completed: false,
+			},
+			actions: { setEditing: jest.fn() },
+		};
+
+		jest.spyOn(CheckBox, 'default').mockReturnValue(<div role="checkBox"/>);
+		jest.spyOn(RemoveButton, 'default')
+			.mockReturnValue(<div role="removeButton"/>);
+		jest.spyOn(TodoManager, 'getEditClassName')
+			.mockReturnValue('edit-inactive');
+
+		const { getByRole } = render(Todo(context));
+
+		fireEvent.click(getByRole('todoText'));
+
+		expect(getByRole('todo')).toBeInTheDocument();
+		expect(getByRole('todo')).toHaveClass('todo-active');
+
+		expect(context.actions.setEditing).toHaveBeenCalledWith(context.data);
+		expect(TodoManager.getEditClassName).toHaveBeenCalledWith(context);
+		expect(CheckBox.default).toHaveBeenCalledWith(context, {});
+		expect(RemoveButton.default).toHaveBeenCalledWith(context, {});
+	});
+
+	test('when the completed is false', () => {
+		const context = {
+			state: { editing: null },
+			data: {
+				todo: Symbol('todo'),
+				completed: true,
+			},
+			actions: { setEditing: jest.fn() },
+		};
+
+		jest.spyOn(CheckBox, 'default').mockReturnValue(<div role="checkBox"/>);
+		jest.spyOn(RemoveButton, 'default')
+			.mockReturnValue(<div role="removeButton"/>);
+		jest.spyOn(TodoManager, 'getEditClassName')
+			.mockReturnValue('edit-inactive');
+
+		const { getByRole } = render(Todo(context));
+
+		fireEvent.click(getByRole('todoText'));
+
+		expect(getByRole('todo')).toBeInTheDocument();
+		expect(getByRole('todo')).toHaveClass('todo-completed');
+
+		expect(context.actions.setEditing).toHaveBeenCalledWith(context.data);
+		expect(TodoManager.getEditClassName).toHaveBeenCalledWith(context);
+		expect(CheckBox.default).toHaveBeenCalledWith(context, {});
+		expect(RemoveButton.default).toHaveBeenCalledWith(context, {});
+	});
+});
