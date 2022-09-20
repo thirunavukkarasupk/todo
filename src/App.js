ed1e5b8af9e8e49720e1f689a4a455821003cb71
KumarThirunavukkarasu <thirunavukkarasupk@gmail.com>
2022-09-20 17:40:48 +0530

    TestCompletedForApp.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-
 import { React, useEffect } from 'react';
 import './App.scss';
 import TodoPane from './component/TodoPane/Index';
--- src/App.test.js
+++ src/App.test.js
@@ -1,12 +1,24 @@
 import { render } from '@testing-library/react';
-
+import React from 'react';
 import App from './App';
 import context from './core/context';
+import TaskManager from './services/TaskManager';
+import Ticker from './services/Ticker';
+import * as TaskPane from './component/TaskPane/Index';
+import * as TodoPane from './component/TodoPane/Index';
 
-describe('App', () => {
-	test('renders the component appropriately', () => {
-		const component = render(App(context)).getByRole('App');
+test('renders the component appropriately', () => {
+	jest.spyOn(React, 'useEffect');
+	jest.spyOn(TaskManager, 'init').mockReturnValue();
+	jest.spyOn(Ticker, 'start').mockReturnValue();
+	jest.spyOn(TodoPane, 'default').mockReturnValue(<div role="todoPane"/>);
+	jest.spyOn(TaskPane, 'default').mockReturnValue(<div role="taskPane"/>);
 
-		expect(component).toBeInTheDocument();
-	});
+	const { getByRole } = render(<App { ...context }/>);
+
+	expect(getByRole('App')).toBeInTheDocument();
+	expect(getByRole('todoPane')).toBeInTheDocument();
+	expect(TodoPane.default).toHaveBeenCalledWith(context, {});
+	expect(getByRole('taskPane')).toBeInTheDocument();
+	expect(TaskPane.default).toHaveBeenCalledWith(context, {});
 });
